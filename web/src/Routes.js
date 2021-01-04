72ca7146dc92872753028227e084bf0c454cf114
Greg Murray <44830296+gmmurray@users.noreply.github.com>
2021-01-04 16:09:29 -0500

    changed crud routes to be admin only

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,24 +1,26 @@
-import { Router, Route } from '@redwoodjs/router';
+import { Router, Route, Private } from '@redwoodjs/router';
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/user-list-items/new" page={NewUserListItemPage} name="newUserListItem" />
-      <Route path="/user-list-items/{id:Int}/edit" page={EditUserListItemPage} name="editUserListItem" />
-      <Route path="/user-list-items/{id:Int}" page={UserListItemPage} name="userListItem" />
-      <Route path="/user-list-items" page={UserListItemsPage} name="userListItems" />
-      <Route path="/list-items/new" page={NewListItemPage} name="newListItem" />
-      <Route path="/list-items/{id:Int}/edit" page={EditListItemPage} name="editListItem" />
-      <Route path="/list-items/{id:Int}" page={ListItemPage} name="listItem" />
-      <Route path="/list-items" page={ListItemsPage} name="listItems" />
-      <Route path="/user-lists/new" page={NewUserListPage} name="newUserList" />
-      <Route path="/user-lists/{id:Int}/edit" page={EditUserListPage} name="editUserList" />
-      <Route path="/user-lists/{id:Int}" page={UserListPage} name="userList" />
-      <Route path="/user-lists" page={UserListsPage} name="userLists" />
-      <Route path="/lists/new" page={NewListPage} name="newList" />
-      <Route path="/lists/{id:Int}/edit" page={EditListPage} name="editList" />
-      <Route path="/lists/{id:Int}" page={ListPage} name="list" />
-      <Route path="/lists" page={ListsPage} name="lists" />
+      {/* <Private unauthenticated="home"> */}
+      <Route path="/admin/user-list-items/new" page={NewUserListItemPage} name="newUserListItem" />
+      <Route path="/admin/user-list-items/{id:Int}/edit" page={EditUserListItemPage} name="editUserListItem" />
+      <Route path="/admin/user-list-items/{id:Int}" page={UserListItemPage} name="userListItem" />
+      <Route path="/admin/user-list-items" page={UserListItemsPage} name="userListItems" />
+      <Route path="/admin/list-items/new" page={NewListItemPage} name="newListItem" />
+      <Route path="/admin/list-items/{id:Int}/edit" page={EditListItemPage} name="editListItem" />
+      <Route path="/admin/list-items/{id:Int}" page={ListItemPage} name="listItem" />
+      <Route path="/admin/list-items" page={ListItemsPage} name="listItems" />
+      <Route path="/admin/user-lists/new" page={NewUserListPage} name="newUserList" />
+      <Route path="/admin/user-lists/{id:Int}/edit" page={EditUserListPage} name="editUserList" />
+      <Route path="/admin/user-lists/{id:Int}" page={UserListPage} name="userList" />
+      <Route path="/admin/user-lists" page={UserListsPage} name="userLists" />
+      <Route path="/admin/lists/new" page={NewListPage} name="newList" />
+      <Route path="/admin/lists/{id:Int}/edit" page={EditListPage} name="editList" />
+      <Route path="/admin/lists/{id:Int}" page={ListPage} name="list" />
+      <Route path="/admin/lists" page={ListsPage} name="lists" />
+      {/* </Private> */}
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
