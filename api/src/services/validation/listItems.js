83c04489d1aaf18d40beafb40deebf72a2122349
Greg Murray <44830296+gmmurray@users.noreply.github.com>
2021-01-05 15:28:50 -0500

    began working on backend model validation

---
+++ api/src/services/validation/listItems.js
@@ -0,0 +1,21 @@
+import { UserInputError } from '@redwoodjs/api';
+import { validate } from 'isbn-util';
+import {
+  INVALID_INPUT,
+  REQUIRED_MESSAGE,
+} from 'src/services/validation/messages';
+
+export const validateListItemCreation = input => {
+  const messages = {};
+
+  if (!input.listId) {
+    messages['listId'] = [REQUIRED_MESSAGE];
+  }
+
+  if (!input.isbn) messages['isbn'] = [REQUIRED_MESSAGE];
+  if (input.isbn && !validate(input.isbn))
+    messages['isbn'] = [INVALID_INPUT('isbn')];
+  if (Object.keys(messages).length > 0) {
+    throw new UserInputError('Cannot create new list item', messages);
+  }
+};
---
+++ api/src/services/validation/messages.js
@@ -0,0 +1,2 @@
+export const REQUIRED_MESSAGE = 'is a required field';
+export const INVALID_INPUT = field => `is an invalid ${field}`;
